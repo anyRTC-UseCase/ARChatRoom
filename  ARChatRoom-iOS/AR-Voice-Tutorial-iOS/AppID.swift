4378dde2151d072466d9467cce6d219c9962fcaa
LinShunCC <71861324+LinShunCC@users.noreply.github.com>
2020-09-27 14:33:45 +0800

    launch screen

---  ARChatRoom-iOS/AR-Voice-Tutorial-iOS/AppID.swift
+++  ARChatRoom-iOS/AR-Voice-Tutorial-iOS/AppID.swift
@@ -9,4 +9,4 @@
 /* AppID
 * anyRTC 为 App 开发者签发的 App ID。每个项目都应该有一个独一无二的 App ID。如果你的开发包里没有 App ID，请从anyRTC官网(https://www.anyrtc.io)申请一个新的 App ID
 */
-let AppID: String = "4e8aa4d4ba22fbd9df596b7b1fc66330"
+let AppID: String = ""
