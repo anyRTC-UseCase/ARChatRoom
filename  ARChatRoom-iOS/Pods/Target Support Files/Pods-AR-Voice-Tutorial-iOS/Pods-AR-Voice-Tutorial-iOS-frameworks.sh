09a9fb1bba193b600b1b93a59aec0e04aa2f85b6
jihua <yangjihua2011@126.com>
2021-04-06 19:02:07 +0800

    update iOS
    修复已知问题；sdk 更新。

---  ARChatRoom-iOS/Pods/Target Support Files/Pods-AR-Voice-Tutorial-iOS/Pods-AR-Voice-Tutorial-iOS-frameworks.sh
+++  ARChatRoom-iOS/Pods/Target Support Files/Pods-AR-Voice-Tutorial-iOS/Pods-AR-Voice-Tutorial-iOS-frameworks.sh
@@ -197,6 +197,8 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${PODS_ROOT}/ARtcKit_iOS/ARtcKit/ARtcKit.framework"
+  install_framework "${PODS_ROOT}/ARtmKit_iOS/ARtmKit/ARtmKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
@@ -204,6 +206,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/XHToast/XHToast.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${PODS_ROOT}/ARtcKit_iOS/ARtcKit/ARtcKit.framework"
+  install_framework "${PODS_ROOT}/ARtmKit_iOS/ARtmKit/ARtmKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
